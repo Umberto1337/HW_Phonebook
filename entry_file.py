59a04c6dec21dafa21b18aa20b38217811439f5a
Alexey <cappuccino13371@gmail.com>
2023-07-08 10:23:00 +0300

    Добавил проверку номера телефона entry_file

--- entry_file.py
+++ entry_file.py
@@ -1,8 +1,18 @@
+import re
+
 def add_entry():
     name = input("Введите ФИО: ")
-    phone_number = input("Введите номер телефона: ")
+    while True:
+        phone_number = input("Введите номер телефона: ")
+        if re.match(r'^(\+7|8)[\d]{10}$', phone_number):
+            if phone_number.startswith('8'):
+                phone_number = '+7' + phone_number[1:]
+            break
+        else:
+            print("Неправильный формат номера. Введите номер телефона в формате +7XXXXXXXXXX или 8XXXXXXXXXX.")
     with open("phonebook.txt", "a", encoding="utf-8") as file:
         file.write(f"{name},{phone_number}\n")
     print("Запись добавлена.")
 
 add_entry()
+
